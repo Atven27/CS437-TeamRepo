0767cded6d40355c7eda9e3bbc27e7e63a49ce74
Sindhuri Devathi <sindhuridevathi@Sindhuris-MacBook-Pro.local>
2023-09-02 17:19:07 -0700

    obstacle avoid code

--- Lab 1/Part 1/picard-code/examples/lab1a-obstacle.py
+++ Lab 1/Part 1/picard-code/examples/lab1a-obstacle.py
@@ -1,27 +1,32 @@
 import picar_4wd as fc
+import time
 
 speed = 30
 
 def main():
     while True:
-        scan_list = fc.scan_step(35)
-        if not scan_list:
+        scan_list = fc.scan_step(45)
+        print('Scan List: {}'.format(scan_list))
+        if not scan_list:        
             continue
-
+        
         tmp = scan_list[3:7]
-        print(tmp)
+        print('tmp: {}'.format(tmp))
         if tmp != [2,2,2,2]:
             # Obstacle is detected
             fc.stop()
-            fc.backward(20)
-            fc.turn_right(15) # need to see what speed value correlates to a 90 degree turn
-            fc.forward(20)
-            fc.turn_left(15) # need to see what speed value correlates to a 90 degree turn
+            time.sleep(0.01)
+            fc.backward(speed)
+            time.sleep(0.01)
+            fc.turn_left(speed)
+            time.sleep(0.01)
         else:
             fc.forward(speed)
+            time.sleep(0.01)
 
 if __name__ == "__main__":
     try: 
         main()
     finally: 
         fc.stop()
+
